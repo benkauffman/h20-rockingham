637e44a05ede6876aa7e81ff2cc783b815dc74b5
Ben Kauffman <ben.kauffman@developmentnow.com>
2016-10-01 08:13:24 -0700

    first commit

---
+++ src/main/java/com/krashidbuilt/Main.java
@@ -0,0 +1,12 @@
+package com.krashidbuilt;
+
+/**
+ * Created by Ben Kauffman on 09/30/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
